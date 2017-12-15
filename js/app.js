244379ef6e57629ac1a0064849efcccbcc2a024b
Andrew Abalos <andysaurusrex21@gmail.com>
2017-12-15 07:47:54 -0800

    first commit

---
+++ js/app.js
@@ -0,0 +1,124 @@
+let name = prompt (`Greetings adventurer! What is your name?`);
+let gender = prompt (`What gender is your character?`);
+let race = prompt (`What race is your character?(Humar, Elf, Dwarf, Lizard, Lalafell)`);
+let characterRole = prompt (`What is your character's starting class? (Warrior, Ranger, Rouge, Cleric, Wizard, Witch?`);
+
+const mainChar = new Hero (name, 10, gender, race, characterRole);
+
+checkRace(mainChar, mainChar.race);
+checkClass(mainChar, mainChar.characterRole);
+
+console.log(mainChar);
+
+class BaseCharacter {
+    constructor (name, health, skills = {attack, mAttack, sneak, persuade}){
+        this.name = name;
+        this.currentHealth = health;
+        this.maxHealth = health;
+        this.barriers = {
+            attack: 10,
+            mAttack: 10,
+            sneak: 10,
+            persuade: 10
+        };
+
+        this.skills = skills;
+                
+    };
+};
+
+
+
+// Hero class
+class Hero extends BaseCharacter {
+    constructor(name, health, gender, skills, race, role){
+        super(name, health, skills);
+        this.name = name;
+        this.currentHealth = health;
+        this.maxHealth = health;
+        this.skills = skills;
+
+        this.isIncapacitated = false;
+
+        this.gender = gender;
+        this.race = race;
+        this.characterRole = role;
+
+        //-- Equipment -- //
+        this.equippedWeapon = {
+            name: `None`,
+            minDamage: null,
+            maxDamage: null
+        }
+        this.equippedArmor = {
+            name: `None`,
+            hitBarrierBonus: null
+        };
+
+        
+    };
+};
+
+// -- Skills for character class and race -- //
+
+// Checks class
+const checkClass = (hero, characterClass) => {
+    let lowerCharacterClass = characterClass.toLowerCase();
+
+        switch (lowerCharacterClass) {
+            case `warrior`:
+                hero.skills.attack += 3;
+                // break statement breaks the code from running again
+                break;
+            case `ranger`:
+                hero.skills.attack ++;
+                hero.skills.sneak ++;
+                hero.skills.persuade ++;
+                break;
+            case `rouge`:
+                hero.skills.attack +=2;
+                hero.skills.sneak +=2;
+                break;
+            case `cleric`:
+                hero.skills.attack ++;
+                hero.skill.mAttack ++;
+                hero.skills.persuade ++;
+                break;
+             // case will continue until break, this is the same as or in this instance
+            case `wizard`:
+            case `witch`:
+                hero.skills.mAttack += 3;
+                break;        
+            default: 
+                characterClass = prompt (`"${characterClass}" is not a valid option. Please choose one!`);
+                hero.characterRole = characterClass;
+                checkClass(hero, characterClass);
+                break;
+    };
+};
+
+// Checks race
+const checkRace = (hero, race) => {
+    let lowerCharacterRace = race.toLowerCase();
+
+        switch (lowerCharacterRace){
+            case 'human':
+            case 'dwarf':
+                hero.skills.attack ++;
+                break;
+            case 'elf':
+                hero.skills.mAttack ++;
+             break;
+            case 'lizard':
+                hero.skills.sneak ++;
+                break;
+            case 'lalafell':
+                hero.skils.persuade ++;
+                break;
+            default:
+                race = prompt (`${race} is not a valid race. Please choose a valid option!`);
+                hero.race = race;
+                checkRace(hero, race);
+                break;
+    };
+};
